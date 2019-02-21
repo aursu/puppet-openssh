c710b5f9fc8d46c86dcef2e858deaefbe9a423fb
Alexander Ursu <alexander.ursu@gmail.com>
2019-02-21 16:14:39 +0100

    templates and unit tests

---
+++ spec/classes/configs_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe 'openssh::configs' do
+  let(:pre_condition) do
+    <<-PRECOND
+    class {'openssh': }
+    PRECOND
+  end
+
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+
+      context 'check sshd config' do
+        it {
+          is_expected.to contain_file('/etc/ssh/sshd_config')
+            .with_content(%r{Port 22})
+          is_expected.to contain_file('/etc/ssh/sshd_config')
+            .with_content(%r{PermitRootLogin without-password})
+          is_expected.to contain_file('/etc/ssh/sshd_config')
+            .with_content(%r{StrictModes yes})
+          is_expected.to contain_file('/etc/ssh/sshd_config')
+            .with_content(%r{GSSAPIAuthentication yes})
+          is_expected.to contain_file('/etc/ssh/sshd_config')
+            .with_content(%r{AllowTcpForwarding no})
+          is_expected.to contain_file('/etc/ssh/sshd_config')
+            .with_content(%r{Banner none})
+          is_expected.to contain_file('/etc/ssh/sshd_config')
+            .with_content(%r{AuthorizedKeysFile .ssh/authorized_keys})
+        }
+      end
+    end
+  end
+end
