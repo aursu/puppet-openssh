62187879046bb0430f01478021fd2a6dbda4d8b1
Alexander Ursu <alexander.ursu@gmail.com>
2019-02-21 16:13:08 +0100

    Services mgmt

---
+++ manifests/services.pp
@@ -0,0 +1,29 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include openssh::services
+class openssh::services (
+  Boolean $service_enabled  = true,
+  String  $service_ensure   = running,
+  String  $service_name     = $openssh::params::service_name,
+) inherits openssh::params
+{
+  service { $service_name:
+    ensure     => $service_ensure,
+    hasstatus  => true,
+    hasrestart => true,
+    enable     => $service_enabled,
+  }
+
+  if  $facts['os']['name'] in ['RedHat', 'CentOS'] and
+      $facts['os']['release']['major'] in ['7', '8'] {
+    systemd::dropin_file { 'sshd.service.d/override.conf':
+      filename => 'override.conf',
+      unit     => 'sshd.service',
+      content  => template('openssh/systemd.override.conf.erb'),
+      before   => Service[$service_name],
+    }
+  }
+}
---
+++ spec/classes/services_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe 'openssh::services' do
+  let(:pre_condition) do
+    <<-PRECOND
+    class {'openssh': }
+    PRECOND
+  end
+
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+
+      context 'check service with default parameters' do
+        let(:params) do
+          {
+          }
+        end
+
+        it {
+          is_expected.to contain_service('sshd')
+            .with_ensure('running')
+            .with_enable(true)
+        }
+      end
+
+      if ['redhat-7-x86_64', 'centos-7-x86_64'].include?(os)
+        context 'check service with default parameters' do
+          it {
+            is_expected.to contain_file('/etc/systemd/system/sshd.service.d/override.conf')
+              .with_content(%r{^Restart=always$})
+              .with_content(%r{^RestartSec=30$})
+              .with_content(%r{^StartLimitInterval=0$})
+          }
+        end
+      end
+    end
+  end
+end
