69d6869f46c277d7474b9a3e79d0f56d630d0524
Alexander Ursu <alexander.ursu@gmail.com>
2019-02-21 16:12:02 +0100

    openssh packages management

---
+++ manifests/packages.pp
@@ -0,0 +1,43 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include openssh::packages
+class openssh::packages (
+  String  $package_ensure       = present,
+  String  $client_ensure        = present,
+  String  $server_ensure        = present,
+  String  $package_name         = $openssh::base_package_name,
+  Boolean $manage_client        = $openssh::manage_client_package,
+  Optional[String]
+          $client_package       = $openssh::client_package_name,
+  Boolean $manage_server        = $openssh::manage_server_package,
+  Optional[String]
+          $server_package       = $openssh::server_package_name,
+  Optional[
+    Array[String]
+  ]       $server_dependencies  = $openssh::openssh_server_dependencies,
+)
+{
+  package { $package_name :
+    ensure => $package_ensure,
+  }
+
+  if $manage_client and $client_package {
+    package { $client_package:
+      ensure => $client_ensure,
+    }
+  }
+
+  if $manage_server and $server_package {
+    if $server_dependencies {
+      package { $server_dependencies:
+        ensure => $server_ensure,
+      }
+    }
+    package { $server_package:
+      ensure => $server_ensure,
+    }
+  }
+}
---
+++ spec/classes/packages_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+describe 'openssh::packages' do
+  let(:pre_condition) do
+    <<-PRECOND
+    class {'openssh': }
+    PRECOND
+  end
+
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      it { is_expected.to compile }
+
+      context 'check client package management' do
+        let(:params) do
+          {
+            'manage_client' => true,
+          }
+        end
+
+        it {
+          is_expected.to contain_package('openssh-clients')
+            .with_ensure('present')
+        }
+      end
+
+      context 'check server package management' do
+        let(:params) do
+          {
+            'manage_server' => true,
+          }
+        end
+
+        it {
+          is_expected.to contain_package('openssh-server')
+            .with_ensure('present')
+        }
+
+        if ['redhat-7-x86_64', 'centos-7-x86_64'].include?(os)
+          it {
+            is_expected.to contain_package('initscripts')
+              .with_ensure('present')
+          }
+        end
+      end
+    end
+  end
+end
