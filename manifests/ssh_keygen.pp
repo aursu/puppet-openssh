03cde8e145da59b2fbd348f22da50d18dd3ca375
Alexander Ursu <alexander.ursu@gmail.com>
2019-04-24 16:43:44 +0200

    sshkey_name is mandatory parameter

--- manifests/ssh_keygen.pp
+++ manifests/ssh_keygen.pp
@@ -5,18 +5,16 @@
 # @example
 #   include openssh::ssh_keygen
 class openssh::ssh_keygen (
-  Optional[String]
-          $sshkey_name            = $openssh::keys::sshkey_name,
-  String  $sshkey_user            = $openssh::keys::sshkey_user,
+  String  $sshkey_name            = $openssh::sshkey_name,
+  String  $sshkey_user            = $openssh::sshkey_user,
   Openssh::KeyType
-          $sshkey_type            = $openssh::keys::sshkey_type,
+          $sshkey_type            = $openssh::sshkey_type,
   Stdlib::Unixpath
-          $sshkey_target          = $openssh::keys::sshkey_target,
+          $sshkey_target          = $openssh::sshkey_target,
   Stdlib::Unixpath
-          $sshkey_dir             = $openssh::keys::sshkey_dir,
-
+          $sshkey_dir             = $openssh::sshkey_dir,
   Array[String]
-          $sshkey_options         = $openssh::keys::sshkey_options,
+          $sshkey_options         = $openssh::sshkey_options,
   Boolean $sshkey_generate_enable = false,
   String  $sshkey_ensure          = present,
   Integer $sshkey_bits            = 2048,
@@ -32,7 +30,7 @@ class openssh::ssh_keygen (
     default   => 'rsa',
   }
 
-  if $sshkey_generate_enable and $sshkey_name {
+  if $sshkey_generate_enable {
     # ~/.ssh/id_dsa
     # ~/.ssh/id_ecdsa
     # ~/.ssh/id_ed25519
@@ -48,27 +46,25 @@ class openssh::ssh_keygen (
     }
   }
 
-  if $root_key_export and $sshkey_name and $sshkey_user == 'root' {
-    if $::sshpubkey_root {
-      $sshkey_array = $::sshpubkey_root
+  if $root_key_export and $sshkey_user == 'root' and $::sshpubkey_root {
+    $sshkey_array = $::sshpubkey_root
 
-      if $sshkey_type in $sshkey_array and $sshkey_name in $sshkey_array {
-        $sshkey_export = $sshkey_array[1]
-      }
-      else {
-        $sshkey_export = undef
-        warning("Can't parse root ssh public key from ${::fqdn}")
-      }
+    if $sshkey_type in $sshkey_array and $sshkey_name in $sshkey_array {
+      $sshkey_export = $sshkey_array[1]
+    }
+    else {
+      $sshkey_export = undef
+      warning("Can't parse root ssh public key from ${::fqdn}")
+    }
 
-      if $sshkey_export {
-        @@ssh_authorized_key { $sshkey_name:
-          ensure  => $sshkey_ensure,
-          key     => $sshkey_export,
-          user    => $sshkey_user,
-          target  => $sshkey_target,
-          options => $sshkey_options,
-          type    => $sshkey_type,
-        }
+    if $sshkey_export {
+      @@ssh_authorized_key { $sshkey_name:
+        ensure  => $sshkey_ensure,
+        key     => $sshkey_export,
+        user    => $sshkey_user,
+        target  => $sshkey_target,
+        options => $sshkey_options,
+        type    => $sshkey_type,
       }
     }
   }
