84aa1c9ec64891b9878fe9c46b247e4b7d707392
Alexander Ursu <alexander.ursu@gmail.com>
2020-08-05 16:21:23 +0200

    File.read except IO.read

--- lib/facter/sshpubkey_root.rb
+++ lib/facter/sshpubkey_root.rb
@@ -1,9 +1,9 @@
 Facter.add('sshpubkey_root') do
   setcode do
     if File.exist?('/root/.ssh/id_rsa.pub')
-      IO.read('/root/.ssh/id_rsa.pub').split(%r{\s+}, 3)
+      File.read('/root/.ssh/id_rsa.pub').split(%r{\s+}, 3)
     elsif File.exist?('/root/.ssh/id_dsa.pub')
-      IO.read('/root/.ssh/id_dsa.pub').split(%r{\s+}, 3)
+      File.read('/root/.ssh/id_dsa.pub').split(%r{\s+}, 3)
     else
       nil
     end
