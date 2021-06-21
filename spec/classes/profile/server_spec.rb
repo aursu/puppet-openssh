438dd399b085b2ede9e3da513c62c13d0bf48148
Alexander Ursu <alexander.ursu@gmail.com>
2021-06-21 23:51:58 +0300

    Added unit test for custom_ssh_keys parameter

--- spec/classes/profile/server_spec.rb
+++ spec/classes/profile/server_spec.rb
@@ -8,6 +8,25 @@ describe 'openssh::profile::server' do
       let(:facts) { os_facts }
 
       it { is_expected.to compile }
+
+      context 'when setup host keys' do
+        let(:params) do
+          {
+            custom_ssh_keys: [
+              {
+                type: 'ssh-rsa',
+                key: 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDRzUkiTj2vWKNrdQtXV8KIVQ74+mBkS5E+fKIdRy6F/8N79Jqli0sx64O916YOGOwD+G7kRjjtXBVcL9xZ/Ur+OCpgAXgEGF8mPnP+mIBofE+OCxXnSbiLFuEiZybyh7MEiHpFaAn7pTLrbuU6gLO2nIs3bnM6FDYajoM0vhZPkKwuRUfE5gNG5sGDDYD2fhwafFsGBjnMjH0atakrKno9lQXkQMDg/hvELgHbbSglq3/h9R2gtKVk6R+tCBn+7b9iKFS2lw3vvb90uVwSOpLIOxefJ1acBspKzRFJrrDP2Obp3jLEzL+fSWGWXudGKsS0NkcV15p2wtRaTEAt9ryf',
+                name: 'Generated-by-Nova',
+              },
+            ],
+          }
+        end
+
+        it {
+          is_expected.to contain_file('/root/.ssh/authorized_keys')
+            .with_content(%r{^ssh-rsa AAAAB3NzaC1yc2EAAAADA.*fSWGWXudGKsS0NkcV15p2wtRaTEAt9ryf Generated-by-Nova$})
+        }
+      end
     end
   end
 end
